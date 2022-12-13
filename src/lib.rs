22ad997338a5c6e6c032b1894f2a863bc23da21f
RinHizakura <s921975628@gmail.com>
2022-12-14 01:35:52 +0800

    Add interface for i32 parsing
    This feature is still in progress

--- src/lib.rs
+++ src/lib.rs
@@ -7,9 +7,10 @@ mod tests {
     #[test]
     fn test_trim_start() {
         let test_strs = vec!["                  123456", "12345678"];
-        for test_str in test_strs {
-            let s1 = test_str.to_string();
-            let s2 = test_str.to_simd_string();
+
+        for s in test_strs {
+            let s1 = s.to_string();
+            let s2 = s.to_simd_string();
             let s1 = s1.trim_start().to_string();
             let s2 = s2.trim_start().to_string();
             assert_eq!(s1, s2);
@@ -20,12 +21,25 @@ mod tests {
     fn test_trim_match() {
         let test_chs = vec!['@', '+'];
         let test_strs = vec!["@@@@@@@@@@@@@@@@@@123456", "++123456"];
+
         for pair in test_strs.into_iter().zip(test_chs.into_iter()) {
-            let (test_str, test_ch) = pair;
-            let s1 = test_str.to_string();
-            let s2 = test_str.to_simd_string();
-            let s1 = s1.trim_start_matches(test_ch).to_string();
-            let s2 = s2.trim_start_matches(test_ch).to_string();
+            let (s, ch) = pair;
+            let s1 = s.to_string();
+            let s2 = s.to_simd_string();
+            let s1 = s1.trim_start_matches(ch).to_string();
+            let s2 = s2.trim_start_matches(ch).to_string();
+            assert_eq!(s1, s2);
+        }
+    }
+
+    #[test]
+    fn test_parse() {
+        let test_strs = vec!["12345678", "98765432"];
+        for s in test_strs {
+            let s1 = s.to_string();
+            let s2 = s.to_simd_string();
+            let s1 = s1.parse::<i32>();
+            let s2 = s2.parse::<i32>();
             assert_eq!(s1, s2);
         }
     }
--- src/simd_string.rs
+++ src/simd_string.rs
@@ -1,6 +1,10 @@
 use std::arch::x86_64::*;
+use std::num::ParseIntError;
 
-/* Reference: https://lemire.me/blog/2017/01/20/how-quickly-can-you-remove-spaces-from-a-string/ */
+/* Reference:
+ * - https://lemire.me/blog/2017/01/20/how-quickly-can-you-remove-spaces-from-a-string/
+ * - https://vgatherps.github.io/2022-11-28-dec/
+ * */
 pub struct SimdString {
     /* FIXME: Would it be better to own the slice instead of the String? */
     s: String,
@@ -48,6 +52,10 @@ impl SimdString {
     pub fn trim_start(&self) -> &str {
         self.trim_start_matches(' ')
     }
+
+    pub fn parse<F: FromSimdStr>(&self) -> Result<F, F::Err> {
+        FromSimdStr::from_str(self)
+    }
 }
 
 impl From<&str> for SimdString {
@@ -65,5 +73,50 @@ impl ToSimdString for str {
     #[inline]
     fn to_simd_string(&self) -> SimdString {
         SimdString::from(self)
+    }
+}
+
+pub trait FromSimdStr: Sized {
+    type Err;
+
+    fn from_str(s: &SimdString) -> Result<Self, Self::Err>;
+}
+
+impl FromSimdStr for i32 {
+    type Err = ParseIntError;
+
+    fn from_str(s: &SimdString) -> Result<Self, Self::Err> {
+        // TODO: consider sign for the number
+        let len = s.s.len();
+        // TODO: support the string with length doesn't align to 8
+        if (len < 8) || (len & 0x7 != 0) {
+            panic!();
+        }
+
+        let src: &[u8] = s.s.as_bytes();
+        let mut pos: usize = 0;
+        let mut i = 0;
+        unsafe {
+            while i < len {
+                let x = _mm_loadu_si128(src.as_ptr().offset(i as isize) as *const _);
+
+                /* Make '0'..'9' become 0..9, otherwise the char will become garbage
+                 * which could be checked then. */
+                //let x = x - '0';
+
+                /* For every packed u8 integers, check whether it is less equal to 9
+                 *
+                 * FIXME: It looks like that we don't have stable API like
+                 * _mm_cmplt_epu8 or _mm_cmpgt_epu8 now. Instead, we complete
+                 * the similar behavior by:
+                 * 1. For each u8 number, doing max(number, 9)
+                 * 2. If this is a valid number, all numbers should equal to 9 */
+                //let max_of_nine = _mm_max_epu8(x, 9);
+
+                todo!()
+            }
+
+            Ok(0)
+        }
     }
 }
